bf7b160d2debbb2a9c1356cb02ab4bfac8be5979
Aleksey_Bessonov <Lexx-88@mail.ru>
2022-12-05 21:01:26 +0100

    add code to programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void print_array(string[] new_array_for_print)
+{
+    Console.Write("[");
+    for (int i = 0; i < new_array_for_print.Length-1; i++)
+    {
+        Console.Write($"{new_array_for_print[i]}, ");
+    }
+    Console.Write($"{new_array_for_print[new_array_for_print.Length-1]}");
+    Console.Write("]");
+}
+
+string[] stringArray = {"12","258","Sunday", "mo", "for", "rurur"};
+
+int newStringArrayLength = 0;
+for (int i = 0; i < stringArray.Length; i++)
+{
+     if (stringArray[i].Length <= 3)
+     {
+        newStringArrayLength +=1;
+     }
+}
+
+string[] shortStringArray = new string[newStringArrayLength];
+
+int count = 0;
+
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        for (int j = 0+count; j < shortStringArray.Length; j++)
+        {
+            shortStringArray[j] = stringArray[i];
+        }
+      count+=1;
+    }
+}
+
+print_array(shortStringArray);
